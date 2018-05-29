0fa8cf92e50f17794ce268e0bfebc31e73800168
thecrimsoncoder <sm860129@wcupa.edu>
2018-05-28 21:31:54 -0400

    Initial commit

---
+++ arabicToRoman.py
@@ -0,0 +1,8 @@
+
+
+def programTitle():
+    print("++++++++++++++++++++++++++++++++++++++++")
+    print("+    a r a b i c T o R o m a n . p y   +")
+    print("+     Created By: Sean McElhare        +")
+    print("+     github.com/thecrimsoncoder       +")
+    print("++++++++++++++++++++++++++++++++++++++++")
